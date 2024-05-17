a5f3ff20c90332420082be10ab902d0077854087
Dongseon Yang <78548833+dongseon0@users.noreply.github.com>
2024-05-17 15:51:05 +0900

    DS : Update_seat.sql

---
+++ update_seat.sql
@@ -0,0 +1,13 @@
+-- 좌석현황 업데이트
+/* 예약 완료 누르면(새로운 reservation이 생성되면) trigger를 사용하여 
+   예약된 좌석의 seatId와 flightId를 확인하여 좌석의 isAvailable 을 true로 바꾼다.*/
+DELIMITER //
+CREATE TRIGGER UpdateSeatIsAvailableAfterReservation
+AFTER INSERT ON reservation
+FOR EACH ROW
+BEGIN
+	UPDATE Seat
+    SET isAvailable = true
+    WHERE flightId = NEW.flightId AND seatNum = NEW.seatNum;
+END //
+DELIMITER ;
