241e37ef947bc708e7dc46c510334dc31a1a6014
Dongseon Yang <78548833+dongseon0@users.noreply.github.com>
2024-05-17 15:27:20 +0900

    DS: Update Reservation table pk, fk, domain

--- ERACLE.sql
+++ ERACLE.sql
@@ -16,16 +16,20 @@ CREATE TABLE `Flight` (
 	`flightPrice`	INT	NOT NULL
 );
 
-CREATE TABLE `Reservation` (
-	`reservationId`	BIGINT	NOT NULL,
-	`flightId`	BIGINT	NOT NULL,
-	`passengerId`	BIGINT	NOT NULL,
-	`passengerNum`	VARCHAR	NOT NULL,
-	`reservationDate`	DATETIME	NOT NULL,
-	`classType`	VARCHAR	NOT NULL,
-	`seatNum`	INT	NOT NULL,
-	`additionalBaggage`	BOOLEAN	NOT NULL,
-	`totalPrice`	INT	NOT NULL
+CREATE TABLE Reservation (
+	reservationId	BIGINT	NOT NULL,
+	flightId	BIGINT	NOT NULL,
+	passengerId	BIGINT	NOT NULL,
+	passengerNum	VARCHAR(10)	NOT NULL,
+	reservationDate	DATETIME	NOT NULL,
+	classType	VARCHAR(5)	NOT NULL,
+	seatNum		INT	NOT NULL,
+	additionalBaggage	BOOLEAN	NOT NULL,
+	totalPrice	INT	NOT NULL,
+	primary key(reservationId),
+	foreign key(flightId) references Flight(flightId),
+	foreign key(passengerId) references Passenger(passengerId),
+	foreign key(passengerNum) references Passenger(passengerNum)
 );
 
 CREATE TABLE `Airline` (
@@ -58,41 +62,3 @@ CREATE TABLE Passenger (
 	address		VARCHAR(50)	NOT NULL,
 	phoneNum	VARCHAR(20)	NOT NULL
 );
-
-ALTER TABLE `Airport` ADD CONSTRAINT `PK_AIRPORT` PRIMARY KEY (
-	`airportId`
-);
-
-ALTER TABLE `Flight` ADD CONSTRAINT `PK_FLIGHT` PRIMARY KEY (
-	`flightId`
-);
-
-ALTER TABLE `Reservation` ADD CONSTRAINT `PK_RESERVATION` PRIMARY KEY (
-	`reservationId`
-);
-
-ALTER TABLE `Airline` ADD CONSTRAINT `PK_AIRLINE` PRIMARY KEY (
-	`airlineId`
-);
-
-ALTER TABLE `Aircraft` ADD CONSTRAINT `PK_AIRCRAFT` PRIMARY KEY (
-	`aircraftId`
-);
-
-ALTER TABLE `Seat` ADD CONSTRAINT `PK_SEAT` PRIMARY KEY (
-	`seatId`,
-	`flightId`
-);
-
-ALTER TABLE `Passenger` ADD CONSTRAINT `PK_PASSENGER` PRIMARY KEY (
-	`passengerId`,
-	`passengerNum`
-);
-
-ALTER TABLE `Seat` ADD CONSTRAINT `FK_Flight_TO_Seat_1` FOREIGN KEY (
-	`flightId`
-)
-REFERENCES `Flight` (
-	`flightId`
-);
-
