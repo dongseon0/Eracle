ca4bb077d5af9afffe7d6694c70e20c4e32a72e5
girim <gilmeee@naver.com>
2024-05-18 11:43:47 +0900

    GR : UPDATE createschema.sql

--- createschema.sql
+++ createschema.sql
@@ -1,76 +1,74 @@
 ﻿CREATE TABLE Airport (
-	airportId	BIGINT	NOT NULL,
-	airportName	VARCHAR(50)	NOT NULL,
-	nation	VARCHAR(50)	NOT NULL,
-	location(50)	VARCHAR	NOT NULL,
-	primary key(airportId)
-);
-
-CREATE TABLE Flight (
-	flightId BIGINT	NOT NULL,
-	departureAirportId BIGINT	NOT NULL,
-	arrivalAirportId BIGINT	NOT NULL,
-	aircraftId	BIGINT	NOT NULL,
-	departureTime	DATETIME	NOT NULL,
-	arrivalTime	DATETIME	NOT NULL,
-	airline	VARCHAR(50) NOT NULL,
-	flightPrice	INT	NOT NULL,
-	primary key(flightId),
-    foreign key(departureAirportId) references Airport(airportId),
-    foreign key(arrivalAirportId) references Airport(airportId),
-    foreign key(aircraftId) references Aircraft(aircraftId)
-
-);
-
-CREATE TABLE Reservation (
-	reservationId	BIGINT	NOT NULL,
-	flightId	BIGINT	NOT NULL,
-	passengerId	BIGINT	NOT NULL,
-	passengerNum	VARCHAR(10)	NOT NULL,
-	reservationDate	DATETIME	NOT NULL,
-	classType	VARCHAR(5)	NOT NULL,
-	seatNum		INT	NOT NULL,
-	additionalBaggage	BOOLEAN	NOT NULL,
-	totalPrice	INT	NOT NULL,
-	primary key(reservationId),
-	foreign key(flightId) references Flight(flightId),
-	foreign key(passengerId) references Passenger(passengerId),
-	foreign key(passengerNum) references Passenger(passengerNum)
+    airportId BIGINT NOT NULL,
+    airportName VARCHAR(50) NOT NULL,
+    nation VARCHAR(50) NOT NULL,
+    location VARCHAR(50) NOT NULL,
+    PRIMARY KEY (airportId)
 );
 
 CREATE TABLE Airline (
-	airlineId	BIGINT	NOT NULL,
-	airlineName	VARCHAR(50)	NOT NULL,
-	primary key(airlineId)
+    airlineId BIGINT NOT NULL,
+    airlineName VARCHAR(50) NOT NULL,
+    PRIMARY KEY (airlineId)
 );
 
 CREATE TABLE Aircraft (
-	aircraftId	BIGINT	NOT NULL,
-	airlineId	BIGINT	NOT NULL,
-	model	VARCHAR(50)	NOT NULL,
-	capacity	INT	NOT NULL,
-	primary key(aircraftId),
-    foreign key(airlineId) references Airline(airlineId)
+    aircraftId BIGINT NOT NULL,
+    airlineId BIGINT NOT NULL,
+    model VARCHAR(50) NOT NULL,
+    capacity INT NOT NULL,
+    PRIMARY KEY (aircraftId),
+    FOREIGN KEY (airlineId) REFERENCES Airline (airlineId)
 );
 
-CREATE TABLE Seat (
-	seatId	BIGINT	NOT NULL,
-	flightId	BIGINT	NOT NULL,
-	seatNum	INT	NOT NULL,
-	isAvailable	BOOLEAN	NOT NULL,
-	primary key(seatId),
-    foreign key(flightId) references Flight(flightId)
+CREATE TABLE Flight (
+    flightId VARCHAR(10) NOT NULL,
+    departureAirportId BIGINT NOT NULL,
+    arrivalAirportId BIGINT NOT NULL,
+    aircraftId BIGINT NOT NULL,
+    departureTime DATETIME NOT NULL,
+    arrivalTime DATETIME NOT NULL,
+    airline VARCHAR(50) NOT NULL,
+    flightPrice INT NOT NULL,
+    PRIMARY KEY (flightId),
+    FOREIGN KEY (departureAirportId) REFERENCES Airport (airportId),
+    FOREIGN KEY (arrivalAirportId) REFERENCES Airport (airportId),
+    FOREIGN KEY (aircraftId) REFERENCES Aircraft (aircraftId)
 );
 
 CREATE TABLE Passenger (
-	passengerId	BIGINT	NOT NULL,
-	passengerNum	VARCHAR(10)	NOT NULL,
-	firstName	VARCHAR(20)	NOT NULL,
-	lastName	VARCHAR(20)	NOT NULL,
-	dateOfBirth 	DATE	NOT NULL,
-	gender		VARCHAR(6)	NOT NULL,
-	nationality	VARCHAR(20)	NOT NULL,
-	address		VARCHAR(50)	NOT NULL,
-	phoneNum	VARCHAR(20)	NOT NULL,
-	primary key(passengerId)
+    passengerId BIGINT NOT NULL,
+    passengerNum VARCHAR(10) NOT NULL,
+    firstName VARCHAR(20) NOT NULL,
+    lastName VARCHAR(20) NOT NULL,
+    dateOfBirth DATE NOT NULL,
+    gender VARCHAR(6) NOT NULL,
+    nationality VARCHAR(20) NOT NULL,
+    address VARCHAR(50) NOT NULL,
+    phoneNum VARCHAR(20) NOT NULL,
+    PRIMARY KEY (passengerId)
+);
+
+CREATE TABLE Seat (
+    seatId BIGINT NOT NULL,
+    flightId VARCHAR(10) NOT NULL,
+    seatNum INT NOT NULL,
+    isAvailable BOOLEAN NOT NULL,
+    PRIMARY KEY (seatId),
+    FOREIGN KEY (flightId) REFERENCES Flight (flightId)
+);
+
+CREATE TABLE Reservation (
+    reservationId BIGINT NOT NULL,
+    flightId VARCHAR(10) NOT NULL,
+    passengerId BIGINT NOT NULL,
+    passengerNum VARCHAR(10) NOT NULL,
+    reservationDate DATETIME NOT NULL,
+    classType VARCHAR(5) NOT NULL,
+    seatNum INT NOT NULL,
+    additionalBaggage BOOLEAN NOT NULL,
+    totalPrice INT NOT NULL,
+    PRIMARY KEY (reservationId),
+    FOREIGN KEY (flightId) REFERENCES Flight (flightId),
+    FOREIGN KEY (passengerId) REFERENCES Passenger (passengerId)
 );
