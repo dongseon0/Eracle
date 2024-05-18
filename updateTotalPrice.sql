614c757bdf8d3b77a036e7bdc7712a7884fc8fb1
Dongseon Yang <78548833+dongseon0@users.noreply.github.com>
2024-05-18 21:35:05 +0900

    DS: fix text

--- updateTotalPrice.sql
+++ updateTotalPrice.sql
@@ -1,6 +1,5 @@
 -- 추가수하물 여부에 따라 totalPrice 업데이트
 /* 예약 정보에서 수하물 추가 Y/N를 input으로 입력하면 수하물 추가가 되고, totalprice가 변경되는 메뉴*/
--- 일단 input 받는 것은 GUI에서 된다고 생각하고 코드 작성했습니다. 이후 수정이 필요할 수 있습니다!
 DELIMITER //
 CREATE PROCEDURE AddBaggage(
 	IN p_reservationId BIGINT,
