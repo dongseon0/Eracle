55eca8ba9e907a1c802aeaccf67c5e02332814e2
SilverLluvia <eb0313@ewhain.net>
2024-05-24 19:54:22 +0900

    EB: Modify getCheapestFlightsByDate function

--- Eracle/src/Flight.java
+++ Eracle/src/Flight.java
@@ -40,16 +40,15 @@ public class Flight {
     
     public static List<String> getCheapestFlightsByDate(Connection conn, String departureDate) throws SQLException {
         List<String> flights = new ArrayList<>();
-        String query = "SELECT airline, arrivalAirportId, MIN(flightPrice) AS MinPrice " +
+        String query = "SELECT airline, MIN(flightPrice) AS MinPrice " +
                        "FROM Flight " +
                        "WHERE DATE(departureTime) = ? " +
-                       "GROUP BY airline, arrivalAirportId";
+                       "GROUP BY airline";
         try (PreparedStatement stmt = conn.prepareStatement(query)) {
             stmt.setDate(1, java.sql.Date.valueOf(departureDate));
             try (ResultSet rs = stmt.executeQuery()) {
                 while (rs.next()) {
                     flights.add("Airline: " + rs.getString("airline") + 
-                                ", Airport ID: " + rs.getString("arrivalAirportId") + 
                                 ", Minimum Price: $" + rs.getDouble("MinPrice"));
                 }
             }
